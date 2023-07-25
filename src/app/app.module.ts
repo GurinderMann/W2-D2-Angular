f4438ca3dad7eb5643edfb5b6064a5401e5bfa3f
GurinderMann <manngurinder110@gmail.com>
2023-07-25 16:07:45 +0200

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ActivePostsComponent } from './active-posts/active-posts.component';
+import { InactivePostsComponent } from './inactive-posts/inactive-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ActivePostsComponent,
+    InactivePostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
